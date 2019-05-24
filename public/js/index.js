50f3013e546aff92d6c1612311fbb8abc68cb966
Leonard Zbona <leonard.zbona@uoit.net>
2019-05-24 14:16:52 -0400

    converted ids to classes

--- public/js/index.js
+++ public/js/index.js
@@ -1,41 +1,61 @@
 window.onload = function(){
   
-
-
-  let chat_bttn = document.getElementById("chatbox_bttn");
-  let chat_cntnr = document.getElementById("container");
-  let userInput = document.getElementById("user_input");
-  let messages = document.getElementById("messages")
-
-  // make sure that the input is clear
-  userInput.value = ""
-
+  let chat_bttn = document.getElementsByClassName("chatbox_bttn")
+  let chat_cntnr = document.getElementsByClassName("container")
+  let userInput = document.getElementsByClassName("user_input")
+  let messages = document.getElementsByClassName("messages")
 
   // this is used to open and close chat box
-  chat_bttn.addEventListener('click', () => {
-    console.log("hello");
-    
-    if(chat_cntnr.style.display == 'flex'){
-      chat_cntnr.style.display = 'none'
-    }else{
-      chat_cntnr.style.display = 'flex'
-    }
+  Array.from(chat_bttn).forEach(function(element) {
+    element.addEventListener('click', function (){ 
+      // console.log("works");
+
+      // get the chat conationer
+      Array.from(chat_cntnr).forEach(function(e){
+        // check what displaytype the container is using
+        if(e.style.display == 'flex'){
+          // console.log("true")
+          // disable the container
+          e.style.display = 'none'
+        }else{
+          // enable the container
+          e.style.display = 'flex'
+          // console.log("false")
+        }
+      });
+      
+    });
+  });
+
+  // handle user input 
+  Array.from(userInput).forEach(function(element){
+
+    // clears the chat box on page refresh
+    element.value = ""
+
+    // check for enter key press
+    element.addEventListener('keydown', function(e){
+      
+      if(e.keyCode === 13){
+        // console.log(element.value)
+
+        // create a chat box to store user input
+        let message_box = document.createElement('p');
+
+        // holds user input
+        let textNode = document.createTextNode(element.value)
+        // append userinput to message box
+        message_box.appendChild(textNode)
+        
+        // append messagebox to messages div
+        Array.from(messages).forEach(function (element){
+          element.appendChild(message_box)
+        })
+        // clear userinput box
+        element.value = ""
+      }
+    })
   })
-
-  // this is used to add the new messages to the chat window
-  userInput.addEventListener('keydown', function (e) {
-    if(e.keyCode === 13){
-      console.log(userInput.value)
-      let message_box = document.createElement('p')
-      let textNode = document.createTextNode(userInput.value);
-      message_box.appendChild(textNode)
-      messages.appendChild(message_box)
-
-      userInput.value = ""
-    }
-  })
-
-
-
 
 }
+
