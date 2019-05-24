7e13c6735ef8d967374fbdd24bfad3de6d3d0d27
Web Team <webteam@Webteams-MacBook-Pro.local>
2019-05-24 11:29:47 -0400

    Added user input and chat messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,41 @@
 window.onload = function(){
+  
+
 
   let chat_bttn = document.getElementById("chatbox_bttn");
   let chat_cntnr = document.getElementById("container");
+  let userInput = document.getElementById("user_input");
+  let messages = document.getElementById("messages")
 
+  // make sure that the input is clear
+  userInput.value = ""
+
+
+  // this is used to open and close chat box
   chat_bttn.addEventListener('click', () => {
     console.log("hello");
+    
+    if(chat_cntnr.style.display == 'flex'){
+      chat_cntnr.style.display = 'none'
+    }else{
+      chat_cntnr.style.display = 'flex'
+    }
+  })
 
-    chat_cntnr.setAttribute('style', 'display: flex;');
+  // this is used to add the new messages to the chat window
+  userInput.addEventListener('keydown', function (e) {
+    if(e.keyCode === 13){
+      console.log(userInput.value)
+      let message_box = document.createElement('p')
+      let textNode = document.createTextNode(userInput.value);
+      message_box.appendChild(textNode)
+      messages.appendChild(message_box)
 
-  });
+      userInput.value = ""
+    }
+  })
+
+
+
 
 }
