1e55aca84a6908b8349d22b4e9ed87d9a5de905c
Leonard Zbona <leonard.zbona@uoit.net>
2019-05-31 14:09:36 -0400

    option list updater

--- public/js/index.js
+++ public/js/index.js
@@ -25,44 +25,34 @@ window.onload = function(){
           e.style.display = 'flex'
           // console.log("false")
         }
-        
 
-        
-        Array.from(options_list).forEach(function(element){
-          element.addEventListener('click', function(){
-            Array.from(chat_cntnr).forEach(function(element){
-              element.addEventListener('change', function(){
-                console.log(element.children)
+        if(e.contains(scrolling_chat)){
 
-              })
+          Array.from(options_list).forEach(function(element){
+            element.addEventListener('click', updater)
+              // clearTimeout(updater)
             })
-
-          })
-        })
-         
+          // })
+        }else{
+          console.log("chat box doesnt exist")
+        }
 
       });// end of chat container lop
+
     });// end of  button listener
+
   });// end of button class loop
 
-  // Array.from(chat_cntnr).forEach(function(element){
-  //   element.addEventListener('change', function(){
-  //     if(element.classList.contains(innerMessage)){
-  //       console.log("true1");
-  //       Array.from(innerMessage).forEach(function(element){
-  //         if(element.classList.contains(para)){
-  //           console.log("true2");
-  //         }else{
-  //           console.log("false2");
-  //         }
-  //       })
-  //     }else{
-  //       console.log("false1");
-  //     }
-  //   })
-  // })
+}// end of window onload
 
+function updater(){
+  let chatNodes = scrolling_chat.childNodes;
+
+  // setTimeout(() => {
+    console.log(chatNodes.length)
+    Array.from(chatNodes).forEach(function(element){
+      let newChatNodes = element.childNodes
+      console.log(newChatNodes); 
+    })
+  // },1000)
 }
-
-
-
