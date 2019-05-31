0aebc037f4a353f30c95e31d93e715c887118132
Wose <Jacksonteather@hotmail.com>
2019-05-31 15:03:55 -0400

    simplify event listener attachments
    - changed a couple of classes back to ids since they are _single_ elements that _are_ accessed from JS
    - removed loops since singular elements can be accessed directly
    - options_list is what we really want, don't need refs to scrolling_chat
    - chat_cntr can be referenced directly for styles
    - since chat_bttn will only ever be on the page once, we get it by its id and check for that one button's existence

--- public/js/index.js
+++ public/js/index.js
@@ -1,56 +1,35 @@
 
-let chat_bttn = document.getElementsByClassName("chatbox_bttn")
-let chat_cntnr = document.getElementsByClassName("chat-column")
+let chat_bttn = document.getElementById("chatbox_bttn")
+let chat_cntnr = document.getElementById("chat-column")
 let innerMessage = document.getElementsByClassName("message-inner")
 let options_list = document.getElementsByClassName("options-list")
 let scrolling_chat = document.getElementById("scrollingChat")
 let para = document.getElementsByTagName('p')
 
 window.onload = function(){
-
+  if (chat_bttn) {
   // this is used to open and close chat box
-  Array.from(chat_bttn).forEach(function(element) {
-    element.addEventListener('click', function (){ 
-      // console.log("works");
-
-      // get the chat conationer
-      Array.from(chat_cntnr).forEach(function(e){
-        // check what displaytype the container is using
-        if(e.style.display == 'flex'){
-          // console.log("true")
-          // disable the container
-          e.style.display = 'none'
-        }else{
-          // enable the container
-          e.style.display = 'flex'
-          // console.log("false")
-        }
-
-        if(e.contains(scrolling_chat)){
-
-          Array.from(options_list).forEach(function(element){
-            element.addEventListener('click', updater)
-              // clearTimeout(updater)
-            })
-          // })
-        }else{
-          console.log("chat box doesnt exist")
-        }
-
-      });// end of chat container lop
-
-    });// end of  button listener
-
-  });// end of button class loop
-
+    chat_bttn.addEventListener('click', function (){
+      if(chat_cntnr.style.display === 'flex'){
+        // console.log("true")
+        // disable the container
+        chat_cntnr.style.display = 'none'
+      }else{
+        // enable the container
+        chat_cntnr.style.display = 'flex'
+        // console.log("false")
+      }
+      [...options_list].forEach(element => element.addEventListener('click', updater));
+    });
+  }
 }// end of window onload
 
 function updater(){
   let chatNodes = scrolling_chat.childNodes;
 
   // setTimeout(() => {
-    console.log(chatNodes.length)
-    Array.from(chatNodes).forEach(function(element){
+    console.log(chatNodes.length);
+    [...chatNodes].forEach(function(element){
       let newChatNodes = element.childNodes
       console.log(newChatNodes); 
     })
