7b8abd7c3f94352742bcd40363d949f8e3127258
Leonard Zbona <leonard.zbona@uoit.net>
2019-06-07 15:26:53 -0400

    working on fix for links

--- public/js/common.js
+++ public/js/common.js
@@ -29,19 +29,12 @@ let Common = (function () {
 
     // Fill the "content" of the element
     if (domJson.text) {
+      let regexExp = /https?:\/\/?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)(#[a-z0-9]{1,22})?/gi
 
-      let regexExp = /https?:\/\/?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
-      let regexExp2 = /https?:\/\/?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?#[a-z0-9]{1,22}/gi
-
-      let link;
-
-      if(domJson.text.match(regexExp)){
-        link  = domJson.text.match(regexExp)
-      }else if(domJson.text.match(regexExp2)){
-        link  = domJson.text.match(regexExp2)
-        console.log(link);
-      }
-
+      let link = domJson.text.match(regexExp);
+      
+      console.log(link);
+      
       if(link && link.length){
         link.forEach(function(link_text){
           domJson.text = domJson.text.replace(link_text, '<a href="' + link_text+ '">' + "here" + '</a>')
