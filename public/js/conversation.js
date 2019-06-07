7226c6404a1d2a05ec2b2fc7e69f3d4a9e0927b7
Leo Zbona <leonard.zbona@uoit.net>
2019-06-07 11:30:46 -0400

    Revert "Console logging"

--- public/js/conversation.js
+++ public/js/conversation.js
@@ -82,18 +82,6 @@ let ConversationPanel = (function () {
         });
       }
       setResponse(responses, isUser, chatBoxElement, 0, true);
-
-      /**
-       * Some extremely obnoxious logging
-       */
-      console.groupCollapsed(`New message from ${typeValue}:`);
-      console.group('HTTP response payload:')
-      console.log(newPayload);
-      console.groupEnd();
-      console.group('Rendered responses:')
-      console.log(responses);
-      console.groupEnd();
-      console.groupEnd();
     }
   }
 
